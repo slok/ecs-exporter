bf56ded4c1b603881fa7c1f4a4c9a5b433e15e17
Xabier Larrakoetxea <slok69@gmail.com>
2016-11-30 18:32:05 +0100

    Add main
    Signed-off-by: Xabier Larrakoetxea <slok69@gmail.com>

---
+++ cmd/ecs-exporter/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/prometheus/client_golang/prometheus"
+
+	"github.com/slok/ecs-exporter/collector"
+	"github.com/slok/ecs-exporter/log"
+)
+
+// Main is the application entry point
+func Main() int {
+	log.Infof("Starting ECS exporter...")
+
+	// Parse command line flags
+	if err := parse(os.Args[1:]); err != nil {
+		log.Error(err)
+		return 1
+	}
+
+	if cfg.debug {
+		log.SetLevel(log.DebugLevel)
+	} else {
+
+	}
+
+	// Create the exporter and register it
+	exporter, err := collector.New(cfg.awsRegion)
+	if err != nil {
+		log.Error(err)
+		return 1
+
+	}
+	prometheus.MustRegister(exporter)
+
+	// Serve metrics
+	http.Handle(cfg.metricsPath, prometheus.Handler())
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`<html>
+             <head><title>ECS Exporter</title></head>
+             <body>
+             <h1>ECS Exporter</h1>
+             <p><a href='` + cfg.metricsPath + `'>Metrics</a></p>
+             </body>
+             </html>`))
+	})
+
+	log.Infoln("Listening on", cfg.listenAddress)
+	log.Fatal(http.ListenAndServe(cfg.listenAddress, nil))
+
+	return 0
+}
+
+func main() {
+	// Run main program
+	exCode := Main()
+	os.Exit(exCode)
+}
